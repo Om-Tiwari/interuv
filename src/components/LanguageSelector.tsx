4967c9f889a37e7b0994503cdaf803c060b7bde6
shubham6822 <sharmashubham6822@gmail.com>
2025-01-12 20:01:46 +0530

    language selector

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -6,18 +6,22 @@ import {
   SelectTrigger,
   SelectValue,
 } from "./ui/select";
+import { LANGUAGE_VERSIONS } from "./data";
 
 export default function LanguageSelector() {
+  const language = Object.entries(LANGUAGE_VERSIONS);
   return (
     <div>
       <Select>
         <SelectTrigger className="w-[180px]">
-          <SelectValue placeholder="Theme" />
+          <SelectValue placeholder="Select Language" />
         </SelectTrigger>
         <SelectContent>
-          <SelectItem value="light">Light</SelectItem>
-          <SelectItem value="dark">Dark</SelectItem>
-          <SelectItem value="system">System</SelectItem>
+          {language.map(([lang, version]) => (
+            <SelectItem value={version} key={version}>
+              {lang}
+            </SelectItem>
+          ))}
         </SelectContent>
       </Select>
     </div>
